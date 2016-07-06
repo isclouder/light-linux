cd3de243daa87d10560ccb6a0cdce6ec47b3d6c9
liu_xbing <liu_xbing@massclouds.com>
2016-07-06 14:21:42 +0800

    change kernel from 4.4.0 to 4.3.0

--- mk_x86.sh
+++ mk_x86.sh
@@ -5,7 +5,7 @@ PATH_BROOT="${PATH_CRT}/buildroot-2015.11.1"
 
 build_rootfs(){
     cd ${PATH_BROOT}
-    cp ${PATH_CRT}/config/mcos_config_x86 .config
+    cp ${PATH_CRT}/config/buildroot_config_x86 .config
     make
     [ $? -ne 0 ] && exit 1
 }
